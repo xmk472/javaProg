c851ff1b54e79b5290f480a22524c7f9f5161075
xmk472 <bab63299@gmail.com>
2024-11-04 10:05:20 +0000

    Proramme Java

---
+++ Prog.java
@@ -0,0 +1,20 @@
+public class Prog
+{
+public static void main(String[] args)
+{
+    String s= "As salamou haleykoum" ;
+
+    String d = "Cilonek";
+
+    if(s.compareTo(d) == 0)
+
+    
+    System.out.println("Ces deux chaines sont pareilles en termes d'ecriture");
+else
+
+    System.out.println("Ces deux chaines ne s'ecrivent pas de la meme maniere");
+
+   
+}
+    
+}
